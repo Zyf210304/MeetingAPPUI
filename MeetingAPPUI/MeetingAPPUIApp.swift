05fbfc2bd67f0581a34d27f47476a2f63b4e6fac
张亚飞 <672233580@qq.com>
2021-07-05 22:40:31 +0800

    Initial commit

---
+++ MeetingAPPUI/MeetingAPPUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAPPUIApp.swift
+//  MeetingAPPUI
+//
+//  Created by 张亚飞 on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAPPUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
